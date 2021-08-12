a867b7c57322308d9d8c8566c48a3942f0c9abfc
fowlis <fowlis@protonmail.com>
2021-08-12 02:14:36 +0100

    Update github.js
    Changed github link to the new repo

--- commands/github.js
+++ commands/github.js
@@ -8,7 +8,7 @@ module.exports = {
         if (message.author.id != ownerID) {
             message.reply('you must be the bot owner to run this command, sorry!')
         } else {
-            message.reply(`Here is my GitHub repo! https://github.com/fowlis/CatboyBot`)
+            message.reply(`Here is my GitHub repo! https://github.com/fowlis/CatboyBot-Refined`)
         }
     },
 }
