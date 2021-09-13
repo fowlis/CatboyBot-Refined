b56a6b1c0e246fda64a62cee48b3bb18f844ca08
fowlis <fowlis@protonmail.com>
2021-09-13 10:10:12 +0100

    Create googleforme.js

---
+++ commands/googleforme.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'googleforme',
+    description: 'google term for me',
+    aliases: ['gglfm','gfm'],
+    cooldown: 5,
+    execute(message, args) {
+        if (args == '') {
+            message.channel.send('You need to give me something to Google!')
+            return
+        } else var query = args.join('+')
+        message.channel.send(`http://googleitfor.me/?q=${query}`)
+    },
+}
