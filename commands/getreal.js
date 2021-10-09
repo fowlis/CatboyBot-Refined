4471dc73f55061edb6dd51141ae30368988008e7
fowlis <fowlis@protonmail.com>
2021-10-09 13:25:37 +0100

    Create getreal.js
    get real

---
+++ commands/getreal.js
@@ -0,0 +1,28 @@
+const randomBetween = require('../utils/randomBetween')
+
+module.exports = {
+    name: 'getreal',
+    description: 'get real',
+    cooldown: 5,
+    execute(message) {
+        const gifs = [
+            'https://tenor.com/view/get-real-scott-the-woz-scott-the-woz-get-real-funny-real-gif-23170110',
+            'https://tenor.com/view/get-real-gif-23169318',
+            'https://tenor.com/view/get-real-cat-skate-funny-meme-gif-18666878',
+            'https://tenor.com/view/getreal-gif-18750509',
+            'https://tenor.com/view/get-real-gif-20585094',
+            'https://tenor.com/view/get-real-skating-cat-what-how-what-gif-20810470', // 5
+            'https://tenor.com/view/get-real-the-sound-of-poggers-gif-18701237',
+            'https://tenor.com/view/get-real-gif-21384923',
+            'https://tenor.com/view/sonic-the-hedgehog-sonic-get-real-get-real-gif-19386744',
+            'https://tenor.com/view/get-real-nintendo-funny-gif-18779041',
+            'https://tenor.com/view/bob-fnf-bob-fnf-get-real-get-real-fnf-gif-21665283', // 10
+            'https://tenor.com/view/get-real-trombone-cruel-angels-thesis-gif-20305175',
+            'https://tenor.com/view/porter-robinson-second-sky-air-to-earth-get-real-gif-23206073',
+            'https://tenor.com/view/get-real-watermelon-speed-speed-up-eating-gif-20032777',
+            'https://tenor.com/view/get-real-sexy-among-us-gif-19307656',
+        ]
+
+        message.channel.send(`${gifs[randomBetween(0, 14)]}`)
+    },
+}
